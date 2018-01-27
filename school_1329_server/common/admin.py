874c22ce43f7e58007350d38faa7698da9be2612
potykion <potykion@gmail.com>
2018-01-27 22:29:53 +0300

    RegistrationCode in admin.

--- school_1329_server/common/admin.py
+++ school_1329_server/common/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.models import Group as AuthGroup
 from rest_framework.authtoken.models import Token
 
-from school_1329_server.users.models import User
+from school_1329_server.users.models import User, RegistrationCode
 from school_1329_server.groups.models import Group
 
 admin.site.unregister([
     AuthGroup, Token
 ])
 admin.site.register([
-    User, Group
+    User, Group, RegistrationCode
 ])
